dbaa57c91ba59874eefc7ccda7651942405efb03
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2021-08-23 21:52:33 -0300

    Adiciona código organizado

--- src/main/java/com/testeunitario/crudperson/PessoaRunTimeExceptions.java
+++
@@ -1,6 +0,0 @@
-package com.testeunitario.crudperson;
-
-public class PessoaRunTimeExceptions extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-
-}
--- src/main/java/com/testeunitario/crudperson/services/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.testeunitario.crudperson.services;
-
-import com.testeunitario.crudperson.entities.Book;
-
-public interface BookService {
-	
-	Book save(Book book);
-
-}
