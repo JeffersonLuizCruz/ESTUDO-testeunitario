26806d329050110f176d82895943bd4a13675441
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-06-23 11:47:17 -0300

    refat

--- src/main/java/com/tdd/spring/entities/Book.java
+++ src/main/java/com/tdd/spring/entities/Book.java
@@ -1,10 +1,10 @@
 package com.tdd.spring.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -24,7 +24,6 @@ public class Book {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 	private String title;
 	private String author;
 	private String isbn;
--- src/main/java/com/tdd/spring/repository/BookRepository.java
+++ src/main/java/com/tdd/spring/repository/BookRepository.java
@@ -1,11 +1,9 @@
 package com.tdd.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.tdd.spring.entities.Book;
 
-@Repository
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
