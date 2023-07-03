984928423c3fa3daa00b587dffb92e3483bc2ecf
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-07-02 23:07:43 -0300

    feat: adiciona entidade

--- src/main/java/com/tdd/spring/entity/Customer.java
+++ src/main/java/com/tdd/spring/entity/Customer.java
@@ -1,5 +1,26 @@
 package com.tdd.spring.entity;
 
-public class Customer {
+import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor @NoArgsConstructor
+@Entity
+public class Customer implements Serializable{
+	private static final long serialVersionUID = 900109324855892013L;
+	
+	@Id @EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	private String password;
 }
