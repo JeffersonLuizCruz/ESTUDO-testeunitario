91590844264917dfd2fc8c3953f2f2780e561b55
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-07-28 23:49:48 -0300

    feat: Adicionado teste do método findAll

--- src/test/java/com/tdd/spring/services/CustomerServiceTest.java
+++ src/test/java/com/tdd/spring/services/CustomerServiceTest.java
@@ -3,6 +3,7 @@ package com.tdd.spring.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +22,7 @@ import com.tdd.spring.service.impl.CustomerServiceImpl;
 @SpringBootTest
 public class CustomerServiceTest {
 
+	private static final int INDEX 					= 0;
 	private static final Long ID 					= 1L;
 	private static final String EMAIL 				= "jeff.luiz.cruz@gmail.com";
 	private static final String PASSWORD 			= "123";
@@ -31,14 +33,10 @@ public class CustomerServiceTest {
 	 * mocks que são criados com as anotações @Mock(ou @Spy) nesta instância.
 	 */
 	
-	@InjectMocks 
-	private CustomerServiceImpl customerServiceImpl;
-	@Mock 
-	private CustomerRepository customerRepository;
-	@Mock 
-	private Customer customer;
-	@Mock 
-	private Optional<Customer> optionalCustomer;
+	@InjectMocks private CustomerServiceImpl customerServiceImpl;
+	@Mock private CustomerRepository customerRepository;
+	@Mock private Customer customer;
+	@Mock private Optional<Customer> optionalCustomer;
 	
 	@BeforeEach
 	void setUp() {
@@ -69,6 +67,20 @@ public class CustomerServiceTest {
 		}
 	}
 	
+	@Test
+	void whenFindAllReturnAnListOfCustomers() {
+		when(customerRepository.findAll()).thenReturn(List.of(this.customer));
+		
+		List<Customer> customerEntities = customerServiceImpl.findAll();
+		
+		assertNotNull(customerEntities);
+		assertEquals(1, customerEntities.size());
+		assertEquals(Customer.class, customerEntities.get(INDEX).getClass());
+		
+		assertEquals(ID, customerEntities.get(INDEX).getId());
+		assertEquals(EMAIL, customerEntities.get(INDEX).getEmail());
+		assertEquals(PASSWORD, customerEntities.get(INDEX).getPassword());
+	}
 	void startCustomer() {
 		this.customer = Customer.builder()
 		.id(ID)
