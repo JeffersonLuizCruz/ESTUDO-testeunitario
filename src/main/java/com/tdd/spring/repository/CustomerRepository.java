415dd27b5ba3fa7846ead49303c8579e9168f62d
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-07-29 00:01:39 -0300

    feat: Adiciona cobertura de teste do método save()

--- src/main/java/com/tdd/spring/repository/CustomerRepository.java
+++ src/main/java/com/tdd/spring/repository/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.tdd.spring.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tdd.spring.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
-
+	Optional<Customer> findByEmail(String email);
 }
